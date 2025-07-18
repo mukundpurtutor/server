833095cddbdd7a230986acdb502b54b336964a4c
mrankit5575 <mrankit5575@gmail.com>
2025-07-18 10:06:27 +0530

    udate

--- db/db.js
+++ db/db.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+// Load environment variables
 dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
 
     console.log('✅ MongoDB connected successfully');
   } catch (err) {
